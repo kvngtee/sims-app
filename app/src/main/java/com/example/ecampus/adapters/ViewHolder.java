9542a814594396c40e4d56a96ed3e4c3774b0279
Parables Boltnoel <parables95@gmail.com>
2020-01-10 06:04:21 +0000

    Added Image Cache feature using Picaso Image Library

--- app/src/main/java/com/example/ecampus/adapters/ViewHolder.java
+++ app/src/main/java/com/example/ecampus/adapters/ViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.ecampus.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,12 +10,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ecampus.R;
 import com.example.ecampus.models.News;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import es.dmoral.toasty.Toasty;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
+
+    Context context;
 
     @BindView(R.id.news_title)
     TextView Title;
@@ -34,9 +40,32 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Context ctx, News news) {
+        context = ctx;
         Title.setText(news.getTitle());
         Desc.setText(news.getDesc());
         NewsDate.setText(news.getDate().toString());
-        Picasso.get().load(news.getImage()).placeholder(R.drawable.logo).into(NewsImage);
+        Picasso.get().load(news.getImage()).placeholder(R.drawable.logo).networkPolicy(NetworkPolicy.OFFLINE).into(NewsImage, new Callback() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Picasso.get().load(news.getImage()).placeholder(R.drawable.logo).error(R.drawable.logo).into(NewsImage, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.i("PICASO_CACHE", "Couldn't fetch the image drom the network");
+                        Toasty.error(context, "Couldn't fetch the image drom the network").show();
+                    }
+                });
+
+            }
+        });
     }
 }
