237c6ccc7d3c0f2646e9d64888b17a575bd93942
Parables Boltnoel <parables95@gmail.com>
2020-01-16 15:21:19 +0000

    Search feature tested in TimeTable Activity

--- app/src/main/java/com/example/ecampus/activities/TimetableActivity.java
+++ app/src/main/java/com/example/ecampus/activities/TimetableActivity.java
@@ -3,17 +3,33 @@ package com.example.ecampus.activities;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.text.format.DateFormat;
 import android.view.WindowManager;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ecampus.R;
+import com.example.ecampus.adapters.news.NewsSearchAdapter;
+import com.example.ecampus.models.News;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class TimetableActivity extends AppCompatActivity {
 
     private CardView cardview;
+
+    NewsSearchAdapter newsSearchAdapter;
+
+    NewsfeedActivity newsfeedActivity = new NewsfeedActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +43,52 @@ public class TimetableActivity extends AppCompatActivity {
         cardview = findViewById(R.id.cardview);
 
 
-        cardview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardview.setCardBackgroundColor(Color.parseColor("#6C63Ff"));
-                Intent intent = new Intent(TimetableActivity.this, HomescreenActivity.class);
-                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
-                startActivity(intent);
-            }
+        cardview.setOnClickListener(view -> {
+            cardview.setCardBackgroundColor(Color.parseColor("#6C63Ff"));
+            Intent intent = new Intent(TimetableActivity.this, HomescreenActivity.class);
+            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            startActivity(intent);
         });
 
+        newsSearchAdapter = new NewsSearchAdapter(newsfeedActivity.getNewsOptions(this));
+        RecyclerView newsRCV = findViewById(R.id.newsRCV);
+        newsRCV.setHasFixedSize(true);
+        newsRCV.setLayoutManager(new LinearLayoutManager(this));
+        newsRCV.setAdapter(newsSearchAdapter);
 
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String thisYear = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+                String thisMonth = DateFormat.format("MM", new Date()).toString();
+
+                CollectionReference News =
+                        FirebaseFirestore.getInstance().
+                                collection("news")
+                                .document(thisYear).collection(thisMonth);
+                Query query = News.orderBy("title").startAt(s.trim()).endAt(s.trim() + "\uf8ff");
+                FirestoreRecyclerOptions options = new FirestoreRecyclerOptions.Builder<News>()
+                        .setQuery(query, News.class).build();
+                newsSearchAdapter.updateOptions(options);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                String thisYear = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+                String thisMonth = DateFormat.format("MM", new Date()).toString();
+
+                CollectionReference News =
+                        FirebaseFirestore.getInstance().
+                                collection("news")
+                                .document(thisYear).collection(thisMonth);
+                Query query = News.orderBy("title").startAt(s.trim()).endAt(s.trim() + "\uf8ff");
+                FirestoreRecyclerOptions options = new FirestoreRecyclerOptions.Builder<News>()
+                        .setQuery(query, News.class).build();
+                newsSearchAdapter.updateOptions(options);
+                return true;
+            }
+        });
     }
 }
